345c24007bef22e5c50d78d442aec85ee7e08af5
Roman <116812494+Romchik235@users.noreply.github.com>
2025-06-06 21:13:00 +0300

    Update admin.py

--- Back-End/api/admin.py
+++ Back-End/api/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
-from .models import Category, MenuItem, Order, OrderItem, Review
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Category, MenuItem, Order, OrderItem, Review
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    # Додаємо поле phone у форму редагування
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone',)}),
+    )
+
+    # Додаємо відображення в таблиці
+    list_display = ['username', 'email', 'phone', 'is_staff']
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'rating', 'comment', 'menu_item')
+    search_fields = ('user__username', 'comment')
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Category)
 admin.site.register(MenuItem)
 admin.site.register(Order)
 admin.site.register(OrderItem)
-admin.site.register(Review)
+# admin.site.register(Review)
 
